43450225876374f12ed1cc55bf99f2e05861763a
Palak Bhandari <112636258+Palak-Bhandari@users.noreply.github.com>
2023-07-03 16:58:42 +0530

    Create

---
+++ 2. Data Types.cs
@@ -0,0 +1,20 @@
+// Data types
+
+String name2 = "Palak";
+Console.WriteLine("Name (String): " + name2);
+int no = 12;
+Console.WriteLine("a(int): " + no);
+bool bool_var = true;
+Console.WriteLine("Variable(boolean): " + bool_var);
+char c = 's';
+Console.WriteLine("Character(char): " + c);
+double d = 12.34;
+Console.WriteLine("d(double): " + d);
+float f = 12.34F;
+Console.WriteLine("f(float): " + f);
+dynamic dy = 'w';
+Console.WriteLine("dy(dynamic): " + dy);
+Object obj = 100;
+Console.WriteLine("Obj(Object): " + obj);
+Console.WriteLine();
+Console.WriteLine();
