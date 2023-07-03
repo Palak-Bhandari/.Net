74494402a49b2bac0797f00dd62f6aa7ff7e9aef
Palak Bhandari <112636258+Palak-Bhandari@users.noreply.github.com>
2023-07-03 16:29:49 +0530

    Create

---
+++ First.cs
@@ -0,0 +1,8 @@
+// Hello World
+
+Console.WriteLine("Hello, World!");
+Console.Write("Enter name: ");
+var name = Console.ReadLine();
+Console.WriteLine("Name: " + name);
+Console.WriteLine();
+Console.WriteLine();
